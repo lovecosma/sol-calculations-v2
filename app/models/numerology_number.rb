fa98db49ebf4e6ec210c0641e128712dc8ba0c76
Matthew Williams <williamsmatthew333@gmail.com>
2025-10-04 19:24:58 -0600

    finishes show page v1

--- app/models/numerology_number.rb
+++ app/models/numerology_number.rb
@@ -4,4 +4,13 @@ class NumerologyNumber < ApplicationRecord
 
 	delegate :value, to: :number
 	delegate :name, to: :number_type
+	
+	
+	def match_ids
+	@match_ids ||= matches.map(&:to_i)
+	end
+
+	def mismatch_ids
+	@misatch_ids ||= mismatches.map(&:to_i)
+	end
 end
