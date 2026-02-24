300c6986a6792f1fbd20a809a963030a16c4b4ca
Matthew Williams <williamsmatthew333@gmail.com>
2026-02-23 23:22:01 -0700

    fixes typo on celebrity charts route

--- app/controllers/celebrity_charts_controller.rb
+++ app/controllers/celebrity_charts_controller.rb
@@ -1,7 +1,7 @@
 class CelebrityChartsController < ApplicationController
 
   def index
-    @charts = CelebrityChart.select(:id, :full_name, :birthdate, :profile_path)
+    @charts = CelebrityChart.select(:id, :full_name, :birthdate, :profile_path, :updated_at, :created_at, :type)
                             .includes(chart_numbers: { numerology_number: [:number_type, :number] })
                             .order(created_at: :desc)
                             .page(params[:page])
