930d80154726e0b5ac598ad96381c54b958e7849
Matthew Williams <williamsmatthew333@gmail.com>
2026-03-20 23:34:05 -0400

    fixes about route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :privacy, :terms, :about ]
 
   def about
-    http_cache_forever public: true
+    http_cache_forever(public: true) { render }
   end
 
   def privacy
-    http_cache_forever public: true
+    http_cache_forever(public: true) { render }
   end
 
   def terms
-    http_cache_forever public: true
+    http_cache_forever(public: true) { render }
   end
 end
